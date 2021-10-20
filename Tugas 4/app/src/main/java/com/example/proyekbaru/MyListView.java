aae0f3bbeacb97fb5cfb06bef6491699dd1ab27a
MikhsanA <ikhsananugrah42@gmail.com>
2021-10-20 21:29:26 +0700

    Tugas

---
+++ Tugas 4/app/src/main/java/com/example/proyekbaru/MyListView.java
@@ -0,0 +1,28 @@
+package com.example.proyekbaru;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyListView extends AppCompatActivity {
+    ListView listView;
+    public String bhs_program[] = {"prolog","C++","Pascal","Cobol","Perl","Algo L","Java","PHP","Python","HTMl","CSS"};
+
+    Spinner combo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=(ListView) findViewById(R.id.listdata);
+        combo=(Spinner) findViewById(R.id.combodata);
+
+        ArrayAdapter adapter = new ArrayAdapter( MyListView.this, R.layout.support_simple_spinner_dropdown_item,bhs_program);
+        listView.setAdapter(adapter);
+        combo.setAdapter(adapter);
+    }
+}
